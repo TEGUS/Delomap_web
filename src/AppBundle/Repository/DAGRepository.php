620ca2bebdd25227f713712fe45001aa5c16ea00
tegus <tegus@email.com>
2018-02-03 09:46:21 +0100

    update repository

--- src/AppBundle/Repository/DAGRepository.php
+++ src/AppBundle/Repository/DAGRepository.php
@@ -10,4 +10,11 @@ namespace AppBundle\Repository;
  */
 class DAGRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function listAll()
+    {
+        $qb = $this->createQueryBuilder('v')
+            ->getQuery();
+
+        return $qb->getResult();
+    }
 }
