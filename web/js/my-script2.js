d7eb790a4cae91059a48c6a0379cc00ff6bd9a91
michael <tchotemichael@gmail.com>
2018-02-09 17:46:08 +0100

    manage 09022018

--- web/js/my-script2.js
+++ web/js/my-script2.js
@@ -32,6 +32,42 @@ $(function (){
     //click recherce tdr
     $('#select-tdr').searchableOptionList();
     
+     //click sur le bouton editer TP
+    $('#table-terme-reference').on("click", ".edit", function () {
+        
+        console.log('bonjour le code');
+
+        var id;
+        var select;
+        var nom;
+        var description;
+
+        var row = jQuery(this).closest('tr');
+
+        var i = 0;
+        row.find("td").each(function (cellIndex) {
+            if (i === 0) {
+                id = $(this).html();
+            } else if (i === 1) {
+                select = $(this).html();
+            } else if (i === 2) {
+                nom = $(this).html();
+            }
+            else if (i === 3) {
+                description = $(this).html();
+            }
+            i++;
+        });
+
+        $('#id_tdr').val(id);
+        $('#form-terme-reference select.select').val(select);
+        $('#form-terme-reference input.nom').val(nom);
+        $('#form-terme-reference textarea.description').html(description);
+
+        $('#block-table-terme-reference').addClass('hidden');
+        $('#block-form-terme-reference').removeClass('hidden');
+    });
+    
 });
 
 
