84240fa2f8e129ff0e7a64f3b9b8682da6f30205
real-soothsayer <arthurlekane@gmail.com>
2018-02-22 02:12:26 -0500

    22022018

--- web/js/my-script.js
+++ web/js/my-script.js
@@ -135,6 +135,167 @@ $(function () {
             });
         });
     });
+    //click sur le bouton ajouter procedure TP
+    $('#table-type-prestation').on("click", ".add_proc", function () {
+        
+        var id;
+        var nom;
+        var liste_procs;
+
+        var row = jQuery(this).closest('tr');
+        var i = 0;
+        row.find("td").each(function (cellIndex) {
+            if (i === 0) {
+                id = $(this).html();
+            } else if  (i === 1) {
+                nom = $(this).html();
+            } else if(i === 3) {
+                liste_procs = JSON.parse($(this).html());
+            }
+            i++;
+        });
+
+        var liste_procedures = "";
+        for(key in liste_procs) {
+            var doc = liste_procs[key];
+            liste_procedures += '<tr><td>'+doc.id+'</td><td>'+doc.libelle+'</td><td><a href="#" class="remove" title="Supprimer"><i class="fa fa-times fa-lg fa-red"></i></a></td></tr>';
+        }
+        $('#table-prestation-procedure tbody').html(liste_procedures);
+
+        $('#modal-tp-proc h4').html('Liste des procédures ['+nom+']');
+
+        $('#modal-tp-proc').modal('show');
+
+        $.ajax({
+            'type': 'POST',
+            'url': Routing.generate('find_all_procs'),
+            'dataType': 'JSON',
+            'success': function(result) {
+
+                var procedures = "";
+                //console.log(result);
+                for (var key in result.data) {
+                    var proc = result.data[key];
+                    procedures += '<option value="' + proc.id + '">' + proc.libelle + '</option>';
+                }
+                
+                $('#list_procs').html(procedures);
+                $('#list_procs').selectpicker('refresh');
+
+                //click sur ajout procedure
+                $('#modal-tp-proc .add').click( function() {
+                    
+                    var idtp = id;
+                    var idproc = $('#list_procs').val();
+                    var nomproc = $("#list_procs option:selected" ).text();
+                    
+                    $.ajax({
+                        'type': 'POST',
+                        'url': Routing.generate('update_proc_add_tp', { 'proc': idproc }),
+                        'data': {
+                            tp: idtp
+                        },
+                        'dataType': 'JSON',
+                        'success': function(result) {
+                            //
+                            if (result.data === true) {
+                                $('#table-prestation-procedure tbody').append('<tr><td>'+idproc+'</td><td>'+nomproc+'</td><td><a href="#" class="remove" title="Supprimer"><i class="fa fa-times fa-lg fa-red"></i></a></td></tr>');
+                            } else {
+                                swal({
+                                    title: "Erreur!",
+                                    text: "Verifiez si la procedure n'existe pas deja dans le tableau",
+                                    type: "error",
+                                    timer: 3000
+                                });
+                            }
+                        },
+                        'error': function() {
+                            //
+                            swal({
+                                title: "Erreur!",
+                                text: "Verifiez si la procedure n'existe pas deja dans le tableau",
+                                type: "error",
+                                timer: 3000
+                             });
+                        },
+                        'beforeSend': function() {
+                            $('#modal-tp-proc button.add i').removeClass('hidden');
+                        },
+                        'complete': function() {
+                            $('#modal-tp-proc button.add i').addClass('hidden');
+                        }
+                    });
+                });
+    
+                //click sur le bouton du tableau document
+                $('#table-prestation-procedure .remove').click(function () {
+                    //console.log('ici');
+            
+                    var idtp = id;
+                    var idproc;
+            
+                    var row = jQuery(this).closest('tr');
+            
+                    var i = 0;
+                    row.find("td").each(function (cellIndex) {
+                        if (i === 0) {
+                            idproc = $(this).html();
+                        }
+                        i++;
+                    });
+            
+                    swal({
+                        title: "Attention !",
+                        text: "Voulez-vous vraiment Supprimer cet élément ? ",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Oui",
+                        cancelButtonText: "Non",
+                        closeOnConfirm: false
+                    }, function () {
+            
+                        $.ajax({
+                            'type': 'POST',
+                            'url': Routing.generate('update_proc_remove_tp', { 'proc': idproc }),
+                            'dataType': 'JSON',
+                            'data': {
+                                tp: idtp
+                            },
+                            'success': function (result) {
+                                if (result.data === true) {
+                                    swal.close();
+                                    $(row).remove();
+                                } else {
+                                    swal("Erreur!", "Erreur", "error");
+                                }
+                            },
+                            'error': function () {
+                                swal("Erreur!", "Erreur", "error");
+                            },
+                            'beforeSend': function() {
+                                $('.sweet-alert button.confirm').html("<i class=\"fa fa-spinner fa-spin\"></i> Oui");
+                            },
+                            'complete': function() {
+                                $('.sweet-alert button.confirm').html("Oui");
+                            }
+                        });
+                    });
+                });
+            },
+            'error': function(xhr, status, error) {
+                var err = eval(xhr.responseText );
+                console.log(err);
+                console.log(error);
+            }
+        });
+
+    });
+
+    //fermeture du modal d'ajout de la procedure au type de prestation
+    $('#modal-tp-proc .close_btn').click( function() {
+        table_tp.ajax.reload();
+    });
 
     //click sur le bouton annuler TP
     $('#block-form-type-prestation .cancel').click(function () {
@@ -486,22 +647,66 @@ $(function () {
             'type': 'POST',
             'url': Routing.generate('find_all_procs_by_tp', { 'tp': tp }),
             'dataType': 'JSON',
-            'data': {
-            },
             'success': function (result) {
+                //console.log(result);
                 var procedures = "";
 
                 for (var key in result) {
                     var proc_obj = result[key];
                     procedures += '<option value="' + proc_obj.id + '">' + proc_obj.libelle + '</option>';
                 }
-                $('#list_proc_tp_projet').html(procedures);
-                $('#list_proc_tp_projet').selectpicker('refresh');
+                $('#list_proc_projet').html(procedures);
+                $('#list_proc_projet').selectpicker('refresh');
             }
         });
 
         $('#block-table-projet').addClass('hidden');
         $('#block-form-add-proc-projet').removeClass('hidden');
+    });
+
+    //Annuler fenetre ajout de procédure au projet
+    $('#form-add-proc-projet .cancel').click(function () {
+        $('#block-form-add-proc-projet').addClass('hidden');
+        $('#block-table-projet').removeClass('hidden');
+    });
+    //Enregistrer la fenetre d'ajout de procédure au projet
+    $('#form-add-proc-projet .save').click(function () {
+
+        var id = $('#id_projet_form_proc').val();
+        
+        var url = Routing.generate('update_projet_choose_proc', { 'projet': id });
+        var msg_reussite = "Ajout de procedure réussie";
+        var msg_echec = "Problème de l'ajout de la procedure";
+        
+        $.ajax({
+            'type': 'POST',
+            'url': url,
+            'dataType': 'JSON',
+            'data': {
+                proc: $('#list_proc_projet').val()
+            },
+            'success': function (result) {
+                if (result.data) {
+                    $('#block-form-add-proc-projet').addClass('hidden');
+                    $('#block-table-projet').removeClass('hidden');
+                    swal("Réussi!", msg_reussite, "success");
+
+                    table_projet.ajax.reload();
+                } else {
+                    swal("Erreur!", msg_echec, "error");
+                }
+            },
+            'error': function () {
+                swal("Erreur!", msg_echec, "error");
+            },
+            'beforeSend': function () {
+                $('#block-form-add-proc-projet .save i').removeClass('hidden');
+            },
+            'complete': function () {
+                $('#block-form-add-proc-projet .save i').addClass('hidden');
+            }
+        });
+
     });
 
     //chargement du datatable projet
